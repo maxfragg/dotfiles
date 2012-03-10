777907dc3f246474cc092cd2f0c9d5020c13ec37
Max Krüger <maxfragg@gmail.com>
2012-03-10 21:52:26 +0100

    explicit given files, because its more flexible

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+DOTFILES=< .files
+#"bash_aliases  bashprompt  bashrc  bashscripts  cipbash config/herbstluftwm"
 
 
 cutstring="DO NOT EDIT BELOW THIS LINE"
-for name in *; do
+#for name in *; do
+for name in $DOTFILES; do
   target="$HOME/.$name"
   if [ -e "$target" ]; then
     if [ ! -L "$target" ]; then
@@ -35,35 +38,3 @@ for name in *; do
   fi
 done
 
-
-for name in .config/*; do
-  target="$HOME/$name"
-  if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"`
-      if [ -n "$cutline" ]; then
-  cutline=$((cutline-1))
-        echo "Updating $target"
-        head -n $cutline "$target" > update_tmp
-        startline=`sed '1!G;h;$!d' "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
-        if [ -n "$startline" ]; then
-          tail -n $startline "$name" >> update_tmp
-        else
-          cat "$name" >> update_tmp
-        fi
-        mv update_tmp "$target"
-      else
-        echo "WARNING: $target exists but is not a symlink."
-      fi
-    fi
-  else
-    if [ "$name" != 'install.sh' ]; then
-      echo "Creating $target"
-      if [ -n "$(grep "$cutstring" "$name")" ]; then
-        cp "$PWD/$name" "$target"
-      else
-        ln -s "$PWD/$name" "$target"
-      fi
-    fi
-  fi
-done
