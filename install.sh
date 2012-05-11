b443bf5bdb9202de601161613f66402a00a962b7
Max Krüger <maxfragg@gmail.com>
2012-05-11 22:31:03 +0200

    simplyfid install.sh by removing cutstring solution

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 #read content of files to decide, which files should be installed
 #files contains files for all hosts
-#if filename starts with a "#" it will not be treated as hidden, 
+#if filename starts with a "#" it will not be treated as hidden,
 #and no "." will be added
 #$HOSTNAME contains host specific files
-#$HOSTNAME-not contains files excluded for this host, 
+#$HOSTNAME-not contains files excluded for this host,
 #overwrites everything else
 
 DOTFILES="`cat files`"
@@ -28,9 +28,6 @@ else
 fi
 
 
-
-cutstring="DO NOT EDIT BELOW THIS LINE"
-
 for name in $DOTFILES; do
   if [ `expr index "$name" "#"` == 1 ]; then
   	name=${name/"#"/""}
@@ -38,34 +35,17 @@ for name in $DOTFILES; do
   else
   	target="$HOME/.$name"
   fi
-  #echo "trying to install $target"
-  #continue
+
   if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"`
-      if [ -n "$cutline" ]; then
-	cutline=$((cutline-1))
-        echo "Updating $target"
-        head -n $cutline "$target" > update_tmp
-        startline=`sed '1!G;h;$!d' "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
-        if [ -n "$startline" ]; then
-          tail -n $startline "$name" >> update_tmp
-        else
-          cat "$name" >> update_tmp
-        fi
-        mv update_tmp "$target"
-      else
-        echo "WARNING: $target exists but is not a symlink."
-      fi
+    if [ -L "$target" ]; then
+      echo "OK: $target"
+    else
+      echo "WARNING: $target exists but is not a symlink."
     fi
   else
     if [ "$name" != 'install.sh' ]; then
       echo "Creating $target"
-      if [ -n "$(grep "$cutstring" "$name")" ]; then
-        cp "$PWD/$name" "$target"
-      else
-        ln -s "$PWD/$name" "$target"
-      fi
+      ln -s "$PWD/$name" "$target"
     fi
   fi
 done
